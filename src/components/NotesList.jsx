cf4688f8bb8e8456c10b714e1b2ed27eddf90e6f
Muhammad Rifqi Setiawan <muhammadrifqi142@gmail.com>
2023-11-14 00:25:46 +0700

    fix: fix delete and archive button when search

--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -4,24 +4,27 @@ import NotesItem from "./NotesItem";
 function NotesList({ notes, onDelete, onArchive, onUnarchive }) {
   const regularNotes = notes.filter((note) => !note.archived);
   const archivedNotes = notes.filter((note) => note.archived);
+  const filteredNotes = notes.filter((note) => !note.archived);
 
   return (
     <>
       <div className="notes-list">
-        {regularNotes.length === 0 ? (
+        {filteredNotes.length === 0 ? (
           <p className="notes-list__empty-message">Tidak ada catatan</p>
         ) : (
-          regularNotes.map((note) => (
+          filteredNotes.map((note) => (
             <NotesItem
               key={note.id}
               id={note.id}
               onDelete={onDelete}
               onArchive={onArchive}
+              onUnarchive={onUnarchive}
               {...note}
             />
           ))
         )}
       </div>
+
       <h2>Arsip</h2>
       <div className="notes-list">
         {archivedNotes.length === 0 ? (
